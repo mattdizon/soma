5715b350317552d42bf5aaba4447de34d39b9697
Matt Dizon <mdizon.dev@gmail.com>
2024-08-26 20:30:22 -0500

    remove pnpm

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bff14f5a39f990e5"; }
+/******/ 		__webpack_require__.h = function() { return "497f9b799dde90d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
